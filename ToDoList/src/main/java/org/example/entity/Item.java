bdc6bfae06e9b9356c99a8a7314e2394ca3e138c
clemzouuu <clemkinoa@gmal.com>
2023-11-17 15:47:46 +0100

    .

--- ToDoList/src/main/java/org/example/entity/Item.java
+++ ToDoList/src/main/java/org/example/entity/Item.java
@@ -1,4 +1,67 @@
 package org.example.entity;
 
+import java.util.Date;
+
 public class Item {
+    private String name;
+    private String content;
+    private Date creationDate;
+    private static final int MAX_LENGTH = 1000;
+    private static final long TIME_BETWEEN_ITEMS = 30 * 60 * 1000;
+
+
+    public Item(String name, String content) {
+        this.name = name;
+        this.content = content;
+        this.creationDate = new Date();
+    }
+
+    public boolean isValid() {
+        return isNameValid()
+                && isContentValid()
+                && isCreationDateValid();
+    }
+
+    private boolean isNameValid() {
+        return this.name != null && !this.name.trim().isEmpty();
+    }
+
+    private boolean isContentValid() {
+        return this.content != null && this.content.length() <= MAX_LENGTH;
+    }
+
+    private boolean isCreationDateValid() {
+
+        long currentTime = System.currentTimeMillis();
+        long itemCreationTime = this.creationDate.getTime();
+        long timeDifference = currentTime - itemCreationTime;
+
+        return timeDifference >= TIME_BETWEEN_ITEMS;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
 }
+
